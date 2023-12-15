505140efc63eb1217acb986f93344e3bbbda8fc6
leul8a <114873712+leul8a@users.noreply.github.com>
2023-12-15 13:17:13 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from string import punctuation
 import nltk
 import csv
 
-nltk.download('stopwords')
+# Ensure that you have the stopwords downloaded
+# nltk.download('stopwords')
 from nltk.corpus import stopwords
 
 # Load the list of stopwords
@@ -24,31 +25,69 @@ def get_unique_text(soup):
     texts = set(soup.stripped_strings)  # Use a set to avoid duplicates
     return ' '.join(texts)
 
-# Function to save word counts to file
+# Functions for saving the current and max word counts
 def save_word_counts(word_counts, filename):
     with open(filename, mode='w', newline='', encoding='utf-8') as outfile:
         writer = csv.writer(outfile)
-        for word, count in word_counts.items():
+        for word, count in sorted(word_counts.items()):
             writer.writerow([word, count])
 
+def read_max_counts(filename):
+    try:
+        with open(filename, mode='r', newline='', encoding='utf-8') as infile:
+            reader = csv.reader(infile)
+            return {rows[0]: int(rows[1]) for rows in reader}
+    except FileNotFoundError:
+        return {}
+
+def update_max_counts(current_counts, max_counts):
+    for word, count in current_counts.items():
+        max_counts[word] = max(max_counts.get(word, 0), count)
+    return max_counts
+
+def save_max_counts(max_counts, filename):
+    with open(filename, mode='w', newline='', encoding='utf-8') as outfile:
+        writer = csv.writer(outfile)
+        for word, count in sorted(max_counts.items()):
+            writer.writerow([word, count])
+
+# Function for saving the deltas
+def save_word_deltas(current_counts, previous_counts, filename):
+    deltas = {word: current_counts.get(word, 0) - previous_counts.get(word, 0)
+              for word in set(current_counts) | set(previous_counts)}
+    with open(filename, mode='w', newline='', encoding='utf-8') as outfile:
+        writer = csv.writer(outfile)
+        for word, delta in sorted(deltas.items()):
+            writer.writerow([word, delta])
+
 # Main script execution
-# Replace `while True:` with a loop for a single iteration for testing
-for _ in range(1):
+for _ in range(1):  # Replace with `while True:` for continuous execution
     # Get the url content
-    r = requests.get("http://localhost:8000/sample_website.html")  # Update with your local or actual URL
+    r = requests.get("http://localhost:8000/sample_website.html")  # Update with your URL
     soup = BeautifulSoup(r.content, "html.parser")
 
     # Get unique text from the HTML content
     unique_text = get_unique_text(soup)
 
     # Count words in the unique text
-    word_counts = count_words(unique_text)
+    current_counts = count_words(unique_text)
 
-    # Save word counts
-    save_word_counts(word_counts, "word_counts.txt")
+    # Read the previous word counts for delta calculation
+    previous_counts = read_max_counts("word_counts.txt")
 
-    # No need to sleep since this is a single iteration for testing
+    # Save the current word counts
+    save_word_counts(current_counts, "word_counts.txt")
+
+    # Calculate and save word deltas based on the previous counts
+    save_word_deltas(current_counts, previous_counts, "word_deltas.txt")
+
+    # Read the previous max counts and update them
+    max_counts = read_max_counts("max_word_counts.txt")
+    updated_max_counts = update_max_counts(current_counts, max_counts)
+
+    # Save the updated max counts
+    save_max_counts(updated_max_counts, "max_word_counts.txt")
+
     # time.sleep(10)
 
-# Print message to indicate the script has finished running
-print("Script completed and word counts have been saved.")
+print("Script completed, word counts and deltas have been saved.")
